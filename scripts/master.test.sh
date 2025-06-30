f13a953bc9da51c38984bdf59f98f13c9c5cf006
Eleanor-Dean <eleanor.dean@metoffice.gov.uk>
2025-06-30 12:21:09 +0100

    Stylistic fixes

--- scripts/master.test.sh
+++ scripts/master.test.sh
@@ -46,6 +46,7 @@ leads="2,3,4" # e.g. if month=5 and leads="2,3,4", valid months are JJA (6,7,8)
 area="45,-30,-2.5,60" # sub-area in degrees for area of interest (comma separated N,W,S,E)
 variable="total_precipitation" # variable of interest, typically "2m_temperature" or "total_precipitation"
 location="Morocco" #Current options include 'None' - no borders, 'UK','Morocco' and 'SAU' - Saudi Arabia
+
 # Services in use:
 cat <<EOF > "$parseyml"
 Services:

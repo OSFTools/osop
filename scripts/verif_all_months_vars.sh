61106f0c12270cbe4f23884b32832e16adbeb753
Nick Savage <nicholas.savage@metoffice.gov.uk>
2025-09-01 16:39:20 +0100

    new branch add in files from old one

---
+++ scripts/verif_all_months_vars.sh
@@ -0,0 +1,171 @@
+#!/bin/bash -l
+#SBATCH --qos=normal
+#SBATCH --mem=128G
+#SBATCH --ntasks=8
+#SBATCH --output=master.txt
+#SBATCH --error=master.err
+#SBATCH --time=00-06:00:00
+#SBATCH --export=NONE
+#SBATCH --mail-type=ALL
+# (C) Crown Copyright, Met Office. All rights reserved.
+#
+# This file is part of osop and is released under the BSD 3-Clause license.
+# See LICENSE in the root of the repository for full licensing details.
+
+# Script to calculate download hindcasts, calculate terciles and plot verification measures.
+set -eu
+
+# this conda env gives an error on load, so
+# can't use -u option
+set +u
+conda activate osop 
+set -u
+
+# pick download location
+downloaddir=$SCRATCH/seafoam/data/master/hindcast/downloads
+productsdir=$SCRATCH/seafoam/data/master/hindcast/products
+scoresdir=$SCRATCH/seafoam/data/master/hindcast/scores
+plotdir=$SCRATCH/seafoam/data/master/hindcast/plots
+logdir=$SCRATCH/seafoam/data/master/hindcast/logfiles
+mkdir -p $downloaddir
+mkdir -p $plotdir
+mkdir -p $logdir
+mkdir -p $productsdir
+mkdir -p $scoresdir
+
+# set PYTHONPATH relative to this location
+lib_path=$(pushd ./../lib > /dev/null && pwd && popd > /dev/null)
+set +u
+export PYTHONPATH=$PYTHONPATH:$lib_path
+set -u
+
+#create a yml file to pass dictionary parameters
+parseyml="$downloaddir/parseyml.yml"
+
+# set parameters
+leads="2,3,4" # e.g. if month=5 and leads="2,3,4", valid months are JJA (6,7,8)
+area="45,-30,-2.5,60" # sub-area in degrees for area of interest (comma separated N,W,S,E)
+location="None" #Current options include 'None' - no borders, 'UK','Morocco' and 'SAU' - Saudi Arabia
+
+# Services in use:
+cat <<EOF > "$parseyml"
+Services:
+    ecmwf: 51
+    meteo_france: 9
+    dwd: 22
+    cmcc: 35
+    ncep: 2
+    jma: 3
+    eccc_can: 4
+    eccc_gem5: 5
+    ukmo: 604
+EOF
+echo "YML file created: $parseyml"
+
+# loop over variables of interest, 
+# typically "2m_temperature" or "total_precipitation"
+for variable in total_precipitation 2m_temperature; do
+    # loop over all months of interest
+    for month in $(seq 1 12); do
+        # get ERA5 data
+        set +e
+        python get_era5.py \
+            --month $month \
+            --leads $leads \
+            --area $area \
+            --downloaddir $downloaddir \
+            --variable $variable \
+            > $logdir/era5_log_${variable}.txt 2>&1
+        exitcode=$?
+        set -e
+        if [ $exitcode -eq 0 ]; then
+            echo era5 $variable $month downloaded
+        else
+            echo era5 $variable $month download failed
+        fi
+    done
+done
+
+# loop over variables of interest, 
+# typically "2m_temperature" or "total_precipitation"
+for variable in total_precipitation 2m_temperature; do
+    # loop over all months of interest
+    for month in $(seq 1 12); do
+    # loop over all centres of interest and get data
+        for centre in meteo_france dwd cmcc ncep ukmo ecmwf jma eccc ;do
+            set +e
+            python get_any_hindcast.py \
+                --centre $centre \
+                --month $month \
+                --leads $leads \
+                --area $area \
+                --variable $variable\
+                --downloaddir $downloaddir \
+                > $logdir/download_log_${variable}_${centre}.txt 2>&1
+            exitcode=$?
+            set -e
+            if [ $exitcode -eq 0 ]; then
+                echo $centre $variable $month: download successful
+            else
+                echo $centre $variable $month: download failed
+            fi
+            # compute terciles and anomalies
+            set +e
+            python compute_products.py \
+                --centre $centre \
+                --month $month \
+                --leads $leads \
+                --area $area \
+                --variable $variable \
+                --downloaddir $downloaddir \
+                --productsdir $productsdir \
+                > $logdir/product_log_${variable}_${centre}.txt 2>&1
+            exitcode=$?
+            set -e
+            if [ $exitcode -eq 0 ]; then
+                echo $centre $variable $month: products generated
+            else
+                echo $centre $variable $month: product generation failed
+            fi
+            # calculate verification scores
+            set +e
+            python compute_scores.py \
+                --centre $centre \
+                --month $month \
+                --leads $leads \
+                --area $area \
+                --downloaddir $downloaddir \
+                --scoresdir $scoresdir \
+                --productsdir $productsdir \
+                --variable $variable \
+                > $logdir/verification_log_${variable}_${centre}.txt 2>&1
+            exitcode=$?
+            set -e
+            if [ $exitcode -eq 0 ]; then
+                echo $centre $variable $month: scores generated
+            else
+                echo $centre $variable $month: score generation failed
+            fi
+            # plot scores
+                set +e
+            python plot_verification.py \
+                --location $location \
+                --centre $centre \
+                --month $month \
+                --leads $leads \
+                --area $area \
+                --downloaddir $downloaddir \
+                --scoresdir $scoresdir \
+                --plotdir $plotdir \
+                --variable $variable \
+                > $logdir/plot_log_${variable}_${centre}.txt 2>&1
+            exitcode=$?
+            set -e
+            if [ $exitcode -eq 0 ]; then
+                echo $centre $variable $month: plots generated
+            else
+                echo $centre $variable $month: plot generation failed
+            fi
+        done
+    done
+done

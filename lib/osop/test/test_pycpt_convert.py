bd565a4d1eb811202141c2e7f63c0b6c05c9b937
Nick Savage <6161374+nhsavage@users.noreply.github.com>
2026-05-07 10:05:27 +0100

    Pycpt get pt2 (#122)
    * add new code for this change and fix Te issue
    
    * use fixture for existing test
    
    * add test_prepare_data
    
    * improve the docstring
    
    * instead of changing var names add Te to ignore words
    
    * should be tf not t_f
    
    * add more complete docstring for new module
    
    * remove erronous unit conversion
    
    * add parameters to all docstrings

--- lib/osop/test/test_pycpt_convert.py
+++ lib/osop/test/test_pycpt_convert.py
@@ -18,6 +18,57 @@ import xarray as xr
 import osop.pycpt_convert as convert
 
 
+def test_mean_ensemble(ds_ensemble):
+    """Test Mean ensemble - average the ensembel for each coord."""
+    # Set up false arrays
+    valid_time = pd.to_datetime(["2020-01-01"])
+    forecast_month = [1, 2, 3]
+    lat = [50.0, 51.0]
+    lon = [-1.0, 0.0]
+    number = [2, 3]
+
+    da_out = convert._mean_ensemble(ds_ensemble["var"])
+
+    np.testing.assert_allclose(da_out.values, 0.0)
+    assert "number" not in da_out
+
+
+def test_prepare_data(ds_ensemble):
+    """Test prepare data - check the output dataset has the expected variable and data array."""
+    F, var_name = convert._prepare_data(ds_ensemble)
+
+    assert var_name == "var"
+    assert F.equals(ds_ensemble[var_name])
+
+
+@pytest.fixture
+def ds_ensemble():
+    """Set up a dataset with an ensemble dimension."""
+    valid_time = pd.to_datetime(["2020-01-01"])
+    forecast_month = [1, 2, 3]
+    lat = [50.0, 51.0]
+    lon = [-1.0, 0.0]
+    number = [2, 3]
+
+    ds_ensemble = xr.Dataset(
+        {
+            "var": (
+                ("valid_time", "forecastMonth", "latitude", "longitude", "number"),
+                np.zeros((1, 3, 2, 2, 2)),
+            )
+        },
+        coords={
+            "valid_time": valid_time,
+            "forecastMonth": forecast_month,
+            "latitude": lat,
+            "longitude": lon,
+            "number": number,
+        },
+    )
+
+    return ds_ensemble
+
+
 @pytest.fixture
 def dataset():
     """Create a dataset for testing."""
